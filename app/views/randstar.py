cc7a38f043717e0f1a9972629d1f9b8ba20039cc
rgb-24bit <rgb-24bit@foxmail.com>
2019-01-05 20:22:43 +0800

    fix: fix some potential problems

--- app/views/randstar.py
+++ app/views/randstar.py
@@ -22,6 +22,7 @@ randstar = Blueprint('randstar', __name__)
 
 def make_starred_url(user, page=1):
     """Construct a github interface url that gets the star list."""
+    # TODO The acquisition of the star list is done on the client side.
     url_format = 'https://api.github.com/users/{user}/starred' \
         + '?page={page}&per_page=100'
     return url_format.format(user=user, page=page)
@@ -30,14 +31,18 @@ def make_starred_url(user, page=1):
 @cache.cached(timeout=604800)  # Updated once a week
 def get_user_starred(user):
     """Get the list of stars for the specified user."""
+    # TODO clear empty list cache
     starred, page = [], 1
     while page > 0:
         response = requests.get(make_starred_url(user, page))
 
-        if response.status_code == 404:
+        # 403: Exceeded request limit
+        # 404: User does not exist
+        if response.status_code in (403, 404):
             return []
 
-        if response.json():
+        # Up to 100 items at a time
+        if len(response.json()) == 100:
             starred.extend(response.json())
             page += 1
         else:
@@ -58,4 +63,8 @@ def randstar_user(user):
 @randstar.route('/api/<user>')
 def randstar_api_user(user):
     user_starred = get_user_starred(user)
-    return jsonify(random.choices(user_starred, k=8))
+
+    # random.choices() can't choose empty set
+    if user_starred:
+        return jsonify(random.choices(user_starred, k=8))
+    return jsonify([])
